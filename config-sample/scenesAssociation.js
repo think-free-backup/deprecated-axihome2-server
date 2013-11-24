f53b6a4566ea2c5c705e6a0ff2df35015f09a4ee
christophe <christophe@Naboo.(none)>
2013-11-24 12:44:13 +0100

    New scenes

--- config-sample/scenesAssociation.js
+++ config-sample/scenesAssociation.js
@@ -2,7 +2,19 @@
 	{
 		"name" : "All On",
 		"desc" : "Turn every light on",
-		"scene" : "AllOn",
+		"scene" : "SalonAllOn",
+		"place" : "Salon"
+	},
+	{
+		"name" : "Cinema",
+		"desc" : "Cinema mode",
+		"scene" : "Cinema",
+		"place" : "Salon"
+	},
+	{
+		"name" : "Disco",
+		"desc" : "Disco mode",
+		"scene" : "Dance",
 		"place" : "Salon"
 	}
 ]
